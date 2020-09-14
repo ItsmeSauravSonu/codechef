be12f8b82082393596bb860c3d8a48b62723d290
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2020-09-14 23:42:48 +0530

    Create MUFFINS3.java

---
+++ Beginners/MUFFINS3.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		try{
+		    Scanner scn = new Scanner(System.in);
+		    int t = scn.nextInt();
+		    for(int i=0;i<t;i++){
+		        int n = scn.nextInt();
+		        System.out.println(n/2+1);
+		    }
+		}
+		catch(Exception e){
+		    return;
+		}
+	}
+}
